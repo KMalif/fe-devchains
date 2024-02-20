bdf2bd59a841045760df1557f0d7b744570bcdac
KMalif <khafidulmualif2019@gmail.com>
2024-02-20 16:41:12 +0700

    initial commit

---
+++ src/pages/Home/components/SearchBar/index.jsx
@@ -0,0 +1,12 @@
+import classes from './style.module.scss';
+
+
+const SearhcBar = () => {
+    return (
+        <div className={classes.searchContainer}>
+
+        </div>
+    )
+}
+
+export default SearhcBar;
